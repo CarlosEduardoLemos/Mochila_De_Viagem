4c1c561c6e3835b4f5a76cd02349d5e7e6a86099
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-05-30 22:18:26 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 const form = document.getElementById("novoItem")
+
+form.addEventListener("submit", (evento) =>{
+    evento.preventDefault()
+
+    criaElemento(evento.target.elements('nome').value, evento.target,elements('quantidade').value)
+})
+
+function criaElemento(nome, quantidade){
+
+    //<li class="item"><strong>"quantidade"</strong>item</li>
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    novoItem.innerHTML = quantidade
+}
